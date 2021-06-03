33b29f51cb7af4059c3eb16b7282097935b04859
root <root@DESKTOP-CIPN87P.localdomain>
2021-06-03 14:06:25 -0400

    config/startup.js

--- config/startup.js
+++ config/startup.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 var login = require("../routes/login");
 var register = require("../routes/register");
-var signup = require("../routes/signup");
 
 router.get("/", (req, res) => {
     res.render("index", {});
