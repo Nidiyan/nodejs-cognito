4e3eceece8bc320f876dd5819c18472fe39c3800
root <root@DESKTOP-CIPN87P.localdomain>
2021-06-06 16:10:37 -0400

    Fixed typo in scripts/application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,7 +6,7 @@ sudo chmod -R 777 /home/ec2-user/topictalk-app
 cd /home/ec2-user/topictalk-app
 
 # Add Node and NPM to the path
-export NVM_DIRn="$HOME/.nvm"
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
